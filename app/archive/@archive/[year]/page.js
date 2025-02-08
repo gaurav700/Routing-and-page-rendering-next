fd7bce1ce105a296ec15a9d65a0a970082b28274
fg227 <fg227@njit.edu>
2025-02-08 17:21:30 +0000

    Catch all fallback routes & dealing with multiple path segemnts

--- app/archive/@archive/[year]/page.js
+++
@@ -1,7 +0,0 @@
-import NewsList from "@/components/news-list";
-import { getNewsForYear } from "@/lib/news";
-
-export default function FilterPageNews({params}){
-    const year = params.year;
-    return <NewsList news = {getNewsForYear(year)}/>
-}
