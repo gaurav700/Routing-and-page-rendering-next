a1a01494e7e5d61379f65b0ef05892424d09005b
fg227 <fg227@njit.edu>
2025-02-08 17:29:41 +0000

    Handling errors with components

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -32,6 +32,14 @@ export default function ArchiveFilterPage({ params }) {
     newsContent = <NewsList news={news} />;
   }
 
+  if (
+    (selectedYear && !getAvailableNewsYears().includes(+selectedYear)) ||
+    (selectedMonth &&
+      !getAvailableNewsMonths(selectedYear).includes(+selectedMonth))
+  ) {
+    throw new Error('Invalid filter.');
+  }
+
   return (
     <>
       <header id="archive-header">
