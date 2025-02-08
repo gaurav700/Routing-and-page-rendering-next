3a719d0db0e7f572b3df7e277eabbd659bbb1475
fg227 <fg227@njit.edu>
2025-02-08 18:51:57 +0000

    FInal commit

--- app/(marketing)/layout.js
+++ app/(marketing)/layout.js
@@ -11,7 +11,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <div id="page">
-          <MainHeader />
           {children}
         </div>
       </body>
---
+++ middleware.js
@@ -0,0 +1,5 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request){
+    return new NextResponse.next();
+}
